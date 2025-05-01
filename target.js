62b62fe99aa7bb95d9f13f88296672aa0cdaa2f6
oviesa <opensourceovi@gmail.com>
2025-05-01 11:41:48 -0400

    Update target.js

--- target.js
+++ target.js
@@ -3,9 +3,9 @@ document.addEventListener('DOMContentLoaded', function() {
     if (cartNumbers) {
         try {
             cartNumbers = JSON.parse(cartNumbers);
-            const cartLabel = document.querySelector("#cartlogo label");
-            if (cartLabel) {
-                cartLabel.textContent = cartNumbers;
+            const cartCount = document.querySelector(".cart-count");
+            if (cartCount) {
+                cartCount.textContent = cartNumbers;
             }
         } catch (e) {
             console.error("Error parsing cart numbers:", e);
